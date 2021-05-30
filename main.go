30d519af5aac43b1c5c43946492d5a05da7b65db
AsishMandoi <luckyasish13@gmail.com>
2021-05-31 02:44:47 +0530

    Added few comments in main.go

--- main.go
+++ main.go
@@ -14,22 +14,24 @@ type stu struct {
 }
 
 func AddNewStudent(db *sql.DB, student stu) {
+	// Checking if user with the given student.rollno is present in the DB
 	row := db.QueryRow("SELECT COUNT(*) FROM users WHERE rollno=" + strconv.Itoa(student.rollno))
 	var present bool
 	row.Scan(&present)
+
 	if !present {
 		_, err := db.Exec("INSERT INTO users(rollno, name) VALUES (?, ?)", student.rollno, student.name)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Printf("Added user %v: %v successfully.\n", student.rollno, student.name)
-	} else {
+	} else { // If such a user is already present, just display that message
 		fmt.Printf("User with roll number %v is already present!\n", student.rollno)
 	}
 }
 
 func main() {
-	db, err := sql.Open("sqlite3", "./iitkusers.db")
+	db, err := sql.Open("sqlite3", "./iitkusers.db") // A .db file is created in the same directory which is the database
 	if err != nil {
 		panic(err)
 	}
