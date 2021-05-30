416554ca8e5aafd681bed7c2f48fc76439f27c6b
AsishMandoi <luckyasish13@gmail.com>
2021-05-30 17:01:23 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # IITK Coin
-Task-1 of the project IITK-Coin
+This is a `go` program. Once executed the program connects to a SQLite database named `iitkusers` and creates a table named `users` inside the database. It contains a function that takes details from a user as its arguement and adds them to the database only if they are a new user. If however, the details of an existing user are tried to be added, the function just displays that the user details are already present and does nothing more.
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type stu struct {
+	rollno int
+	name   string
+}
+
+func AddNewStudent(db *sql.DB, student stu) {
+	row := db.QueryRow("SELECT COUNT(*) FROM users WHERE rollno=" + strconv.Itoa(student.rollno))
+	var present bool
+	row.Scan(&present)
+	if !present {
+		_, err := db.Exec("INSERT INTO users(rollno, name) VALUES (?, ?)", student.rollno, student.name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Added user %v: %v successfully.\n", student.rollno, student.name)
+	} else {
+		fmt.Printf("User with roll number %v is already present!\n", student.rollno)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	db, err := sql.Open("sqlite3", "./iitkusers.db")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users(rollno INTEGER not null, name TEXT not null);")
+	if err != nil {
+		panic(err)
+	}
+	AddNewStudent(db, stu{190197, "Someone Cool"})
 }
