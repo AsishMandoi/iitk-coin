7b386437a9df5adab8f60a725787808160c59bfe
Asish Mandoi <luckyasish13@gmail.com>
2021-06-13 07:56:50 +0530

    Task-2: Updated token expiry time to 30 mins

--- server/genToken.go
+++ server/genToken.go
@@ -16,7 +16,7 @@ func GenJWT(payload global.LoginInputFormat) (string, error) {
 
 	// Currently only the rollno and the expiration time are used as claims
 	claims["rollno"] = payload.Rollno
-	claims["exp"] = time.Now().Add(time.Minute * 1).Unix()
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
 	// A token (tokenString) is generated using the header, the payload (using the claims) and the encoded secret key (gobal.SignatureKey)
 	tokenString, err := token.SignedString(global.SignatureKey)
